852dc3ff7876f284a987fc944d8a980203596ce2
3dgoblindev <lorenax230403@gmail.com>
2026-03-05 17:55:09 +0100

    kdj

--- Assets/Scripts/RPG.cs
+++ Assets/Scripts/RPG.cs
@@ -7,10 +7,17 @@ public class RPG : MonoBehaviour
     public float speed = 5f;
 
     private Vector2 movement;
+    private Vector2 last_movement;
+
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -27,5 +34,33 @@ public class RPG : MonoBehaviour
             movement.x = 1;
 
         rb.linearVelocity = movement.normalized * speed;
+
+        if (movement != Vector2.zero)
+        {
+            last_movement = movement;
+            animator.SetBool("IsMoving", true);
+            print("Last Movement: " + last_movement);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+
+        animator.SetFloat("Mov_X", movement.x);
+        animator.SetFloat("Mov_Y", movement.y);
+
+        animator.SetFloat("Last_Mov_X", last_movement.x);
+        animator.SetFloat("Last_Mov_Y", last_movement.y);
+
+        if (movement.x == 1)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movement.x == -1)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+
     }
 }
